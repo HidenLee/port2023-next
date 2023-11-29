c5ed8dc6b7b6f631577b3661124a1dcd38327f93
HidenLee <hyundo1995@gmail.com>
2023-11-29 23:34:20 +0900

    Feat: Header, Intro

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, {useEffect} from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Intro from "@/components/Intro";
@@ -7,8 +8,16 @@ import Site from "@/components/Site";
 import Port from "@/components/Port";
 import Contact from "@/components/Contact";
 import Skip from "@/components/Skip";
+import lenis from "@/utils/lenis";
+import link from "@/utils/link";
 
 export default function Home() {
+
+  useEffect(()=>{
+    lenis();
+    link();
+  },[]);
+
   return (  
   <>
     <Skip />
